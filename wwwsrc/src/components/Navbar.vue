debe66c5aeaf1f0b1a562d166eb41fdd88ed76eb
Cory Brighton <corysairplane@gmail.com>
2019-01-25 09:27:30 -0700

    fix buttons

--- wwwsrc/src/components/Navbar.vue
+++ wwwsrc/src/components/Navbar.vue
@@ -4,31 +4,31 @@
     <!-- Logged in navbar -->
     <div v-if="user.id" class="row">
       <div>
-        <router-link :to="{name: 'home' }"><button class="btn btn-primary">HOME</button></router-link>
+        <router-link :to="{name: 'home' }"><button class="bton">HOME</button></router-link>
       </div>
       <div>
-        <router-link :to="{name: 'dashboard' }"><button class="btn btn-primary">DASHBOARD</button></router-link>
+        <router-link :to="{name: 'dashboard' }"><button class="bton">DASHBOARD</button></router-link>
       </div>
       <div>
-        <button class="btn btn-primary" @click="logout">LOGOUT</button>
+        <button class="bton" @click="logout">LOGOUT</button>
       </div>
     </div>
 
     <!-- Not Logged in navbar -->
     <div v-else class="row">
-      <router-link :to="{name: 'home' }"><button class="btn btn-primary">HOME</button></router-link>
-      <button class="btn btn-primary" v-if="loginForm" @click="loginForm = !loginForm">REGISTER</button>
-      <button class="btn btn-primary" v-else @click="loginForm = !loginForm">LOGIN</button>
+      <router-link :to="{name: 'home' }"><button class="bton">HOME</button></router-link>
+      <button class="bton" v-if="loginForm" @click="loginForm = !loginForm">REGISTER</button>
+      <button class="bton" v-else @click="loginForm = !loginForm">LOGIN</button>
       <form v-if="loginForm" @submit.prevent="loginUser">
         <input type="email" v-model="creds.email" placeholder="email">
         <input type="password" v-model="creds.password" placeholder="password">
-        <button class="btn btn-primary" type="submit">LOGIN</button>
+        <button class="bton" type="submit">LOGIN</button>
       </form>
       <form v-else @submit.prevent="register">
         <input type="text" v-model="newUser.username" placeholder="name">
         <input type="email" v-model="newUser.email" placeholder="email">
         <input type="password" v-model="newUser.password" placeholder="password">
-        <button class="btn btn-primary" type="submit">CREATE ACCOUNT</button>
+        <button class="bton" type="submit">CREATE ACCOUNT</button>
       </form>
     </div>
   </div>
@@ -78,9 +78,13 @@
     justify-content: center;
   }
 
-  .btn {
+  .bton {
     color: white;
     background-color: black;
     border: black;
+  }
+
+  .bton:focus {
+    outline: none;
   }
 </style>
